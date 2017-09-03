5890cdc3e689f4f081eeb3f911abc72e44457542
Jirawong Wongdokpuang <jirawong.wdp@gmail.com>
2017-09-03 10:13:06 +0700

    change menu sidebar url

--- front/components/Sidebar.vue
+++ front/components/Sidebar.vue
@@ -6,86 +6,50 @@
           <span class="fa-fw open-close">
             <i class="ti-close ti-menu"></i>
           </span>
-          <span class="hide-menu">Navigation</span>
+          <span class="hide-menu">เมนู</span>
         </h3>
       </div>
       <ul class="nav" id="side-menu">
         <li v-if="authority === 'Administrator'">
-          <nuxt-link to="/masterdata" class="waves-effect">
+          <nuxt-link to="/checklist/masterdata" class="waves-effect">
             <i class="zmdi zmdi-archive fa-fw"></i>
             <span class="hide-menu">{{ $t('menu.masterdata') }}</span>
           </nuxt-link>
         </li>
         <li v-if="authority === 'Administrator'">
-          <nuxt-link to="/group" class="waves-effect">
+          <nuxt-link to="/checklist/group" class="waves-effect">
             <i class="zmdi zmdi-group fa-fw"></i>
             <span class="hide-menu">{{ $t('menu.group') }}</span>
           </nuxt-link>
         </li>
         <li v-if="authority === 'Administrator' || authority === 'Coordinator'">
-          <nuxt-link to="/category" class="waves-effect">
+          <nuxt-link to="/checklist/category" class="waves-effect">
             <i class="zmdi zmdi-ungroup fa-fw"></i>
             <span class="hide-menu">จัดหมวดหมู่กฎหมาย</span>
           </nuxt-link>
         </li>
         <li v-if="authority === 'Administrator' || authority === 'Coordinator'">
-          <nuxt-link to="/category/list" class="waves-effect">
+          <nuxt-link to="/checklist/category/list" class="waves-effect">
             <i class="zmdi zmdi-collection-bookmark fa-fw"></i>
             <span class="hide-menu">หมวดหมู่กฎหมาย</span>
           </nuxt-link>
         </li>
         <li v-if="authority === 'Owner'">
-          <nuxt-link to="/accord" class="waves-effect">
+          <nuxt-link to="/checklist/accord" class="waves-effect">
             <i class="zmdi zmdi-check-all fa-fw"></i>
             <span class="hide-menu">ประเมินความสอดคล้อง</span>
           </nuxt-link>
         </li>
         <li v-if="authority === 'Approver'">
-          <nuxt-link to="/approve" class="waves-effect">
+          <nuxt-link to="/checklist/approve" class="waves-effect">
             <i class="zmdi zmdi-check-circle-u fa-fw"></i>
             <span class="hide-menu">อนุมัติ</span>
           </nuxt-link>
         </li>
-        <li v-if="false">
-          <nuxt-link to="/coordinator" class="waves-effect">
-            <i class="zmdi zmdi-accounts-alt fa-fw"></i>
-            <span class="hide-menu">{{ $t('menu.coordinator') }}</span>
-          </nuxt-link>
-        </li>
-        <li v-if="false">
-          <nuxt-link to="/owner" class="waves-effect">
-            <i class="zmdi zmdi-account-circle fa-fw"></i>
-            <span class="hide-menu">{{ $t('menu.owner') }}</span>
-          </nuxt-link>
-        </li>
-        <li v-if="false">
-          <nuxt-link to="/reviewer" class="waves-effect">
-            <i class="zmdi zmdi-account-box-o fa-fw"></i>
-            <span class="hide-menu">{{ $t('menu.reviewer') }}</span>
-          </nuxt-link>
-        </li>
-        <li v-if="false">
-          <nuxt-link to="/approver" class="waves-effect">
-            <i class="zmdi zmdi-mood fa-fw"></i>
-            <span class="hide-menu">{{ $t('menu.approver') }}</span>
-          </nuxt-link>
-        </li>
-        <li v-if="false">
-          <nuxt-link to="/uploaddocument" class="waves-effect">
-            <i class="zmdi zmdi-swap-vertical fa-fw"></i>
-            <span class="hide-menu">{{ $t('menu.fileupload') }}</span>
-          </nuxt-link>
-        </li>
         <li v-if="authority === 'Administrator'">
-          <nuxt-link to="/language" class="waves-effect">
+          <nuxt-link to="/checklist/language" class="waves-effect">
             <i class="zmdi zmdi-text-format fa-fw"></i>
             <span class="hide-menu">{{ $t('menu.languageupload') }}</span>
-          </nuxt-link>
-        </li>
-        <li v-if="false">
-          <nuxt-link to="/compliance/add" class="waves-effect">
-            <i class="zmdi zmdi-archive fa-fw"></i>
-            <span class="hide-menu">Compliance Data</span>
           </nuxt-link>
         </li>
       </ul>
