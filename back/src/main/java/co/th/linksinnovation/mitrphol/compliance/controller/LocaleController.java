302952a15f0c0c8e2a22996ffd7ea9fad5a4fbbd
Jirawong Wongdokpuang <jirawong.wdp@gmail.com>
2017-10-20 09:34:40 +0700

    fix lang

--- back/src/main/java/co/th/linksinnovation/mitrphol/compliance/controller/LocaleController.java
+++ back/src/main/java/co/th/linksinnovation/mitrphol/compliance/controller/LocaleController.java
@@ -7,11 +7,11 @@ package co.th.linksinnovation.mitrphol.compliance.controller;
 
 import co.th.linksinnovation.mitrphol.compliance.model.Language;
 import co.th.linksinnovation.mitrphol.compliance.repository.LocaleRespository;
-import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,25 +47,9 @@ public class LocaleController {
     }
     
     @GetMapping("/public/locales/{lang}")
-    public String getLocale(@PathVariable String lang) {
+    public String getLocale(@PathVariable String lang) throws IOException {
         final String jsonFile = "/mnt/locales/"+ lang + ".json";
-        final File file = new File(jsonFile);
-
-        final StringBuilder result = new StringBuilder("");
-        try (Scanner scanner = new Scanner(file)) {
-
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			result.append(line).append("\n");
-		}
-
-		scanner.close();
-
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-        JSONObject jsonResult  = new JSONObject(result.toString());
-	return jsonResult.toString();
+	return new String(Files.readAllBytes(Paths.get(jsonFile)),StandardCharsets.UTF_8);
     }
     
     @DeleteMapping("/api/locales/{lang}")
